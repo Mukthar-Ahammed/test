3041b85357b0ee001dad44f235c1e75ddb254b06
mukthar <mukthar2004@gmail.com>
2026-01-31 16:36:59 +0530

    Fix: correct import path in NewPage.jsx - remove extra space in assets path

--- src/components/NewPage.jsx
+++ src/components/NewPage.jsx
@@ -10,7 +10,7 @@ import TestimonialsCarousel from './TestimonialsCarousel'
 import CTASection from './CTASection'
 import Footer from './Footer'
 import aboutLogo from '../assets/about logo .png'
-import aboutLeft from '../assets/ Images/Web Images/HOMEPAGE/Top Right.jpg'
+import aboutLeft from '../assets/Images/Web Images/HOMEPAGE/Top Right.jpg'
 
 function AboutGlow() {
   return (
